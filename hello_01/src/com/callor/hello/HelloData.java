225c7521dfdaa4d23bb8f76240da2e7257daa817
callor <callor@callor.com>
2023-04-13 11:51:08 +0900

    문자열연산

---
+++ hello_01/src/com/callor/hello/HelloData.java
@@ -0,0 +1,21 @@
+package com.callor.hello;
+
+public class HelloData {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+		System.out.println("Hello" + "Korea");
+		System.out.println("Hello       Korea");
+		System.out.println("Hello"    +      "Korea");
+		
+		System.out.println(30 + 40);
+		System.out.println("30" + "40");
+		System.out.println("30 + 40");
+		
+		// 30 + 40 = 70
+		System.out.println("30 + 40 = 70");
+		System.out.println("30 + 40 = " + 30 + 40);
+		
+	}
+
+}
