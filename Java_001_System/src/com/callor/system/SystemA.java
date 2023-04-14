defe6501f924bd20ae449429e855ef2757038343
callor <callor@callor.com>
2023-04-14 10:45:12 +0900

    System_클래스

---
+++ Java_001_System/src/com/callor/system/SystemA.java
@@ -0,0 +1,19 @@
+package com.callor.system;
+
+// class 이름은 반드시 첫글자 대문자
+// 두단어 이상을 조합하는 것을 권장
+// Camel case 로 작성한다
+public class SystemA {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// System.err.println("반갑습니다");
+		System.out.println();
+		System.out.print(30);
+		System.out.print("+");
+		System.out.print(40);
+		System.out.print("=");
+		System.out.print(30 + 40);
+		System.out.println("안녕하세요");
+		System.out.print(0);
+	}
+}
---
+++ Java_001_System/src/com/callor/system/SystemB.java
@@ -0,0 +1,15 @@
+package com.callor.system;
+
+public class SystemB {
+	
+	public static void main(String[] args) {
+		// form 에 맞도록 데이터를 출력하는 method
+		System.out.printf("%d + %d = %d\n",30,40,30+40);
+		System.out.println();
+		System.out.printf("%d x %d = %d\n",99,77,77*99);
+		System.out.println();
+		System.out.print("\n");
+		System.out.print("대한민국\n우리나라\nRepublic of Korea");
+	}
+
+}
