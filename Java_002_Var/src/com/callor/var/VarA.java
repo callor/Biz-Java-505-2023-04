f5a73ab5f42b45e9f41acb8c44e3ee95471a6da6
callor <callor@callor.com>
2023-04-17 09:48:33 +0900

    var

---
+++ Java_002_Var/src/com/callor/var/VarA.java
@@ -0,0 +1,20 @@
+package com.callor.var;
+
+public class VarA {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		int num2 = 0;
+		
+		num1 = 30;
+		num2 = 40;
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+	}
+
+}
---
+++ Java_002_Var/src/com/callor/var/VarB.java
@@ -0,0 +1,23 @@
+package com.callor.var;
+
+public class VarB {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		int num2 = 0;
+		
+		// 변수에 어떤 값을 저장, 할당, 대입하려면
+		// 반드시 먼저 선언되어야 한다
+		
+		// sum1 선언 하고, 두 변수의 덧셈 결과 저장
+		int sum1 = 0;
+		sum1 = num1 + num2;
+		
+		// sum2 선언과 동시에 두 변수의 덧셈 결과 저장
+		int sum2 = num1 + num2;
+		
+		
+	}
+
+}
